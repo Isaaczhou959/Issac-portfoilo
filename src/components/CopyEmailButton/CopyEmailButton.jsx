c346840ac15aa4bd43d648559e391a1f377c0c00
Issac Zhou <issaczwl@gmail.com>
2025-08-29 16:14:20 +0930

    Add new feature-Copy email button

--- src/components/CopyEmailButton/CopyEmailButton.jsx
+++ src/components/CopyEmailButton/CopyEmailButton.jsx
@@ -1,3 +1,26 @@
+import { Copy } from "lucide-react";
+import { useState } from "react";
+
 export const CopyEmailButton = () => {
-  return <div></div>;
+  const [copied, setCopied] = useState(false);
+  const email = "issaczwl@gmail.com";
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(email);
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    });
+  };
+  return (
+    <button
+      onClick={copyToClipboard}
+      className="relative px-1 py-4 text-sm text-center rounded-full font-bold bg-black w-[12rem] cursor-pointer overflow-hidden"
+    >
+      <p className="flex items-center justify-center text-white gap-2">
+        <Copy />
+        Copy Email Address
+      </p>
+    </button>
+  );
 };
